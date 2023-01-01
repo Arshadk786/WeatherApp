4b94016ae5bb56533834f2875c3687234521bac1
Arshad Khan <arshadkhan7867865422@gmail.com>
2023-01-01 14:46:39 +0530

    created function to get coordinates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react"
 
 function App() {
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
+
+  useEffect(
+    function getLocation() {
+      navigator.geolocation.getCurrentPosition(myLocation)
+      function myLocation(position) {
+        setLat(position.coords.latitude);
+        setLon(position.coords.longitude);
+      }
+      console.log("Latitude = ", lat);
+      console.log("Longitude = ", lon);
+
+    }
+    , [lat, lon]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
